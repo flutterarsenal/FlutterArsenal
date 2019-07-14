2c3d3f9ae5cd4672fe7340ec669f62d5dd1d9740
Sarthak Verma <hisarthakverma@gmail.com>
2019-07-14 17:17:32 +0530

    Moved fromGithub to private repo

--- fromgithub/await-request.js
+++
@@ -1,9 +0,0 @@
-const request = require('request')
-
-module.exports = async (value) =>
-    new Promise((resolve, reject) => {
-        request(value, (error, response, data) => {
-            if (error) reject(error)
-            else resolve(data)
-        })
-    })
--- fromgithub/readme-get.js
+++
@@ -1,7 +0,0 @@
-// https://raw.githubusercontent.com/pauldemarco/flutter_blue/master/README.md
-const request = require("./await-request");
-
-request({
-    method: 'get',
-    url: 'https://raw.githubusercontent.com/pauldemarco/flutter_blue/master/README.md'
-}).then( data => {console.log(data)});
